8910b7da1d711ff2877e2f1215b72324802a6454
karvel-code <velgithinji@gmail.com>
2023-09-04 20:43:40 +0300

    COmmit

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class AppointmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "calculate_end_time for consultation" do
+    appointment = appointments(:consultation_appointment)
+    appointment.calculate_end_time
+    assert_equal Time.parse("2023-09-01 10:20:00"), appointment.end_time
+  end
 end
